752dadca5674c2c4dbd2f569cc69f5a563e3999b
Tornuz <yadavgourav2001@gmail.com>
2021-10-09 21:44:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Web-chat (https://web-chatting.herokuapp.com)
+
+A Web app designed using Web sockets in node.js.
+
+Just open the browser...Add the url.. Login... Start chatting...
+
+The message you send will go along with your login username , time , date...
+
+If multiple people are using it then you can send private messages also by just using  "@" and then typing name of the user. This will send message to that user only. Sounds nice na!!
+
+## Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Suesanz/Web-chat)
+
+# For development purpose
+
+## Prerequisites:
+
+1.Any javascript IDE supporting node js(recommended: Webstorm by Jetbrains).
+
+2.We will be needing Web Sockets npm package for this  but it will be installed from dependecies so no need to worry about this.
+
+## HOW TO SET UP?
+
+Step 1: Run command 
+          *npm install* 
+
+Step 2: Run command  
+          *node server.js* 
+          and open the respective port mentioned in your console.
+
+## Demonstration
+
+Step 1: Open browser and put down the localhost along with the port mentioned in console.
+
+Step 2: Open another window and put the same localhost as that of previous window.
+
+Step 3: Now just login and start sending messages.
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+
+const SERVER_PORT = process.env.PORT || 3333
+const app = express()
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+let socketIdName = {}
+
+io.on('connection', function (socket) {
+    console.log('Socket connected ' + socket.id)
+
+    socket.on('login', (data) => {
+        socketIdName[socket.id] = data.username
+        socket.join(data.username)
+
+        socket.emit('logged_in', {
+            username: data.username,
+            success: true
+        })
+    })
+
+    socket.on('chat', (data) => {
+        if (socketIdName[socket.id]) {
+
+            if (data.message.charAt(0) === '@') {
+
+                let recipient = data.message.split(' ')[0].substring(1)
+
+                io.to(recipient).emit('chat', {
+                    private: true,
+                    sender: socketIdName[socket.id],
+                    message: data.message,
+                    timestamp: new Date()
+                })
+
+            } else {
+                socket.broadcast.emit('chat', {
+                    sender: socketIdName[socket.id],
+                    message: data.message,
+                    timestamp: new Date()
+                })
+            }
+
+
+        }
+    })
+
+
+})
+
+
+app.use('/', express.static(__dirname + '/public'))
+
+server.listen(SERVER_PORT, () => {
+    console.log("Server started on http://localhost:3333")
+})
